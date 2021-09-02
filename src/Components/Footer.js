d97b0e7f551d580f8c6d5f33a99fd782df64a228
awakenthereas0n <lc1816@gmail.com>
2021-09-01 23:19:32 -0700

    Update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2021 LC In Motion</li>
+              <li>&copy; Copyright 2021 LC</li>
            </ul>
 
         </div>
