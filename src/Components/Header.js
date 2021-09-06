5da058ba59b404c101e75fc769850c5309ab3a01
awakenthereas0n <lc1816@gmail.com>
2021-09-05 23:38:21 -0700

    Add box shadow

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,10 +4,6 @@ class Header extends Component {
    render() {
 
       if (this.props.data) {
-         var name = this.props.data.name;
-         var occupation = this.props.data.occupation;
-         var description = this.props.data.description;
-         var city = this.props.data.address.city;
          var networks = this.props.data.social.map(function (network) {
             return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>;
          });
@@ -18,8 +14,11 @@ class Header extends Component {
          <div className="banner-background"/>
          <div className="row banner">
             <div className="banner-text">
-               <h1 className="responsive-headline">Hi, I'm {name}.</h1>
-               <h3>I'm a {city} based <span>{occupation}</span><br />{description}.</h3>
+               <div className="box-shadow">
+                     <h1 className="responsive-headline">Hi, I'm Lauren.</h1>
+                     <h3>I'm a PNW based <span>Systems Architect</span>
+                     <br/>with a passion for creative hobbies and the outdoors.</h3>
+               </div>
             </div>
             <ul className="social">
                {networks}
